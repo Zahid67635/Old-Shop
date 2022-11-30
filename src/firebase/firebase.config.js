0c2e9d8ef6122d21897b7094efd88ec724d1c01c
Zahid <zahid67635gmail.com>
2022-11-30 22:28:59 +0600

    firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyClDthdi6cmxMl3QQbifUb2Kl5OxuBeqi8",
-    authDomain: "old-shop-3cb22.firebaseapp.com",
-    projectId: "old-shop-3cb22",
-    storageBucket: "old-shop-3cb22.appspot.com",
-    messagingSenderId: "983202670393",
-    appId: "1:983202670393:web:1aabcad731d23879bfaae8"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
